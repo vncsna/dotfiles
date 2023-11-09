bd3f1b9536e42702f2bf98e3048ca758c59bf192
Vinicius <vncsna@gmail.com>
2023-11-09 11:19:08 -0300

    feat: add manjaro configuration

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## dotfiles
 
-- general config from [manjaro sway](https://github.com/manjaro-sway)
-- wallpapers from [nordic wallpapers](https://github.com/linuxdotexe/nordic-wallpapers) and [image go nord](https://ign.schrodinger-hat.it/)
+- arch
+  - general config from [manjaro sway](https://github.com/manjaro-sway)
+  - wallpapers from [nordic wallpapers](https://github.com/linuxdotexe/nordic-wallpapers) and [image go nord](https://ign.schrodinger-hat.it/)
 
 remember to add the `nouveau.noaccel=1 quiet` flags to the kernel
---
+++ manjaro/setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -l
+set -e
+
+########################################
+
+if [[ ! "$(pacman -Qe paru)" == *"paru"* ]]; then
+    sudo --user=$USER git clone https://aur.archlinux.org/paru.git
+    (cd paru && sudo --user=$USER makepkg --install --syncdeps --needed --noconfirm)
+    rm -rf paru
+fi
+
+paru --sync --quiet --needed --noconfirm \
+    "aws-cli-v2" \
+    "base-devel" \
+    "bitwarden-cli" \
+    "calibre" \
+    "dbeaver" \
+    "docker" \
+    "docker-compose" \
+    "fzf" \
+    "git" \
+    "git-lfs" \
+    "github-cli" \
+    "man-db" \
+    "man-pages" \
+    "neovim" \
+    "pgcli" \
+    "postgresql" \
+    "redis" \
+    "telegram-desktop" \
+    "terraform" \
+    "terragrunt" \
+    "tldr"
+
+paru --sync --quiet --needed --noconfirm \
+    "1password-cli" \
+    "asdf-vm" \
+    "google-chrome" \
+    "insomnia-bin" \
+    "stremio" \
+    "visual-studio-code-bin" \
+    "zotero-bin"
+
+########################################
+
+git config --global core.editor nvim
+git config --global credential.helper store
+git config --global user.name Vinicius Aguiar
+git config --global user.email vncsna@gmail.com
+
+########################################
+
+sudo cp --backup --recursive ./root/etc/* /etc
+sudo cp --backup --recursive ./root/home/* /home
+
+########################################
+
+install() {
+    if [[ ! "$(which $1)" == *"asdf"* ]]; then
+        asdf plugin add $2 || :
+        asdf install $2 $3
+        asdf global $2 $3
+        asdf reshim $2
+    fi
+}
+source $ASDF_DIR/asdf.sh
+
+install node nodejs 16.15.0
+install python python 3.10.5
+install python python 3.11.16
+install kubectl kubectl 1.22.0
+
+########################################
+
+if [[ ! "$(groups)" == *"docker"* ]]; then
+    sudo groupadd --force docker
+    sudo usermod --append --groups docker $USER
+    newgrp docker
+    systemctl enable docker.service
+    systemctl enable containerd.service
+fi
